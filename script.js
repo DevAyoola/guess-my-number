a63039b1d313ee95200ae2b9a473540cf9e2f097
DavCodec <aydavidfjc@gmail.com>
2023-09-25 14:48:44 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
+
+document.querySelector('.number').value = secretNumber;
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1; 
+
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+    displayMessage("Start guessing...");
+
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('body').style.backgroundColor = '#222';
+});
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+
+    // When there is no input
+    if(!guess) {
+        displayMessage("No Number!");
+    
+        // When player wins
+    } else if (guess === secretNumber) {
+        displayMessage("Correct Number!");
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+
+        if(score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
+        // When guess is wrong
+    } else if (guess !== secretNumber) {
+        if(score > 1) {
+            displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage("You lost the game!");
+            score--;
+            document.querySelector('.score').textContent = 0;
+        }
+    } 
+
+});
